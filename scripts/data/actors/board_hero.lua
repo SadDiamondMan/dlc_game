e356c74365e1af0ec745a645df8fa87e1c965a19
PIZZAROLD <allyff2007@gmail.com>
2026-06-15 20:47:18 -0400

    hero sprite sheet,

--- scripts/data/actors/board_hero.lua
+++ scripts/data/actors/board_hero.lua
@@ -1,6 +1,25 @@
 local actor, super = Class(Actor, "board_hero")
 
 
+function actor:onSpriteInit(sprite)
+    sprite.alpha = 0
+end
+
+
+function actor:onWorldDraw(chara)
+    if self.sheet then
+
+        local a = chara.sprite.sprite_options[1]
+        local b = self.sheet[a]
+        local x, y = b[1] * 16, b[2] * 16
+        local s = self.sprite_sheet
+
+        local quad = Assets.getQuad(x, y, 16, 16, 64, 64)
+        love.graphics.draw(s, quad, 0, 0)
+    end
+end
+
+
 function actor:init()
     super.init(self)
     self.name = "Hero"
@@ -9,7 +28,7 @@ function actor:init()
     self.height = 16
     self.hitbox = {0.2, 8.2, 15.4, 7.4}
     self.soul_offset = {8, 16}
-    self.path = "sword/party/hero"
+    self.path = "sword/party/kris"
     self.default = "walk"
     self.voice = nil
     self.portrait_path = nil
@@ -37,6 +56,23 @@ function actor:init()
     self.healthMax = 90
     self.color = {1, 0.5, 0}
     self.health_color = {1, 0.5, 0}
+
+    self.sprite_sheet = Assets.getTexture("sword/party/hero/sprite_sheet")
+
+    self.sheet = {
+        ["walk/down_1"] = {0, 0},
+        ["walk/down_2"] = {0, 1},
+
+        ["walk/left_1"] = {1, 0},
+        ["walk/left_2"] = {1, 1},
+
+        ["walk/up_1"] = {2, 0},
+        ["walk/up_2"] = {2, 1},
+
+        ["walk/right_1"] = {3, 0},
+        ["walk/right_2"] = {3, 1},
+    }
+
 end
 
 return actor
